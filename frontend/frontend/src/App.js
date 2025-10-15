ade3c5da2c32002753478621c1ef4e6e6391b6a9
alexgorczow <alexgorczow@gmail.com>
2025-10-14 22:00:22 -0500

    Added new event planning page as well as corresponding routing

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Suggestions from "./pages/Suggestions";
+
 
 function App() {
   return (
@@ -10,12 +12,14 @@ function App() {
       <nav>
         <Link to="/">Home</Link> |{" "}
         <Link to="/login">Login</Link> |{" "}
-        <Link to="/signup">Signup</Link>
+        <Link to="/signup">Signup</Link> |{" "}
+        <Link to="/suggestions">Suggestions</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/suggestions" element={<Suggestions />} />
       </Routes>
     </Router>
   );
